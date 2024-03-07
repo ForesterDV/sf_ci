6e2be26d304577cef48af4664a38ef1c2361e0b5
ForesterDV <141860904+ForesterDV@users.noreply.github.com>
2024-03-07 23:38:24 +0500

    Update check_response.sh

--- check_response.sh
+++ check_response.sh
@@ -2,10 +2,10 @@
 response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:9889)
 echo "HTTP Code: $response"
 if [ "$response" == "200" ]; then
-echo "Test sucscess" | mail -s "CI-CD" aaa@gmail.com
+echo "Test sucscess" | mail -s "CI-CD" gdv4admob@gmail.com
 exit 0
 else
 echo "Код ответа: $response"
-echo "Test failed" | mail -s "CI-CD" aaa@gmail.com
+echo "Test failed" | mail -s "CI-CD" gdv4admob@gmail.com
 exit 1
 fi
